a650f46bb8327fe84877327a4943829ac075089c
Chumak Vladimir <parsival427@gmail.com>
2024-04-05 12:14:13 +0300

    Вернул https

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function registration(){
     let credential = await navigator.credentials.create({
         publicKey: {
           challenge: new Uint8Array([117, 61, 252, 231, 191, 241]),
-          rp: { id: "pars1vali.github.io/WebAuth/", name: "ACME Corporation" },
+          rp: { id: "https://pars1vali.github.io/WebAuth/", name: "ACME Corporation" },
           user: {
             id: new Uint8Array([79, 252, 83, 72, 214, 7, 89, 26]),
             name: "jamiedoe",
